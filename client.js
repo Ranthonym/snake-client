9d8ae87580f0f77c03e0bfeee582b31fd0a4fbea
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-12 21:02:06 +0000

    refactored input file to include move commands

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net = require('net');
 
 
+
 /**
  * Establishes connection with the game server
  */
@@ -15,53 +16,10 @@ const connect = function() {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write("Name: RAM");
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 1000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 2000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 3000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 4000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 5000);
-    // setTimeout(() => { 
-    //   conn.write("Move: left" );
-  
-    // }, 6000);
-    // setTimeout(() => { 
-    //   conn.write("Move: left" );
-  
-    // }, 7000);
-    // setTimeout(() => { 
-    //   conn.write("Move: left" );
-  
-    // }, 8000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 9000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 10000);
-    // setTimeout(() => { 
-    //   conn.write("Move: up" );
-  
-    // }, 11000);
+
 
   });
-  
+
   // function to attach an event handler to handle incoming data and console log it for the player.
   conn.on('data', (data) => {
     console.log('game says: ', data);
--- input.js
+++ input.js
@@ -1,19 +1,45 @@
 const connect  = require('./client');
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function() {
-  const  handleUserInput =  (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-   }
-
+const setupInput = function(conn) {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
    
   return stdin;
 };
 
+//function to convert each keyboard input into move commands sent to server
+const  handleUserInput =  (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+       setTimeout(() => { 
+       connection.write("Move: up" );
+     }, 100);
+    }
+     if (key === 'a') {
+      setTimeout(() => { 
+      connection.write("Move: left" );
+    }, 100);
+  }
+    if (key === 's') {
+      setTimeout(() => { 
+      connection.write("Move: down" ); 
+    }, 100);
+  }
+    if (key === 'd') {
+      setTimeout(() => { 
+      connection.write("Move: right" )
+    }, 100);
+  }
+ };
 module.exports = setupInput;
